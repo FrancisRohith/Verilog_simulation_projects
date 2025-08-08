7473dfbcf5d2a597d892498bcec1f887ac56c63c
FrancisRohith <166921745+FrancisRohith@users.noreply.github.com>
2025-08-08 18:14:36 +0530

    Update RAM_memory.v

--- HACK_computer (nand2tetis)/RAM_memory.v
+++ HACK_computer (nand2tetis)/RAM_memory.v
@@ -5,19 +5,22 @@ module Memory(
     input ld,
     output [15:0] out
      );
-     
+    
     wire [15:0] outs [0:2];
     wire ld_sel [0:2];
     
-    assign ld_sel[0] = ld & (address[14:13] == 2'b01);
-    assign ld_sel[1] = ld & (address[14:13] == 2'b10);
-    assign ld_sel[2] = ld & (address[14:13] == 2'b11);
+    assign ld_sel[0] = ld && ((address[14:13] == 2'b00) || (address[14:13] == 2'b01)); // 0 - 16K
+    assign ld_sel[1] = ld && (address[14:13] == 2'b10); // 16K - 24K
+    assign ld_sel[2] = ld && (address == 15'b111111111111111); 
     
     RAM16K data_mem(.clk(clk), .in(in), .address(address[13:0]), .ld(ld_sel[0]), .out(outs[0]));
     SCREEN SCR(.clk(clk), .in(in), .address(address[12:0]), .ld(ld_sel[1]), .out(outs[1]));
     Register KYBD(.clk(clk), .in(in), .ld(ld_sel[2]), .out(outs[2]));
     
-    assign out = outs[address[14]];
+    assign out = ld_sel[0] ? outs[0] : 
+                 ld_sel[1] ? outs[1] : 
+                 ld_sel [2] ? outs[2] : 
+                 15'bz;
     
 endmodule
 
@@ -49,10 +52,10 @@ module RAM16K(
     wire [15:0] outs [0:3];
     wire ld_sel [0:3];
     
-    assign ld_sel[0] = ld & (address[13:12] == 2'b00);
-    assign ld_sel[1] = ld & (address[13:12] == 2'b01);
-    assign ld_sel[2] = ld & (address[13:12] == 2'b10);
-    assign ld_sel[3] = ld & (address[13:12] == 2'b11);
+    assign ld_sel[0] = ld && (address[13:12] == 2'b00);
+    assign ld_sel[1] = ld && (address[13:12] == 2'b01);
+    assign ld_sel[2] = ld && (address[13:12] == 2'b10);
+    assign ld_sel[3] = ld && (address[13:12] == 2'b11);
     
     RAM4K R4K0(.clk(clk), .in(in), .address(address[11:0]), .ld(ld_sel[0]), .out(outs[0]));
     RAM4K R4K1(.clk(clk), .in(in), .address(address[11:0]), .ld(ld_sel[1]), .out(outs[1]));
@@ -73,14 +76,14 @@ module RAM4K(
     wire [15:0] outs [0:7];
     wire ld_sel [0:7];
     
-    assign ld_sel[0] = ld & (address[11:9] == 3'b000);
-    assign ld_sel[1] = ld & (address[11:9] == 3'b001);
-    assign ld_sel[2] = ld & (address[11:9] == 3'b010);
-    assign ld_sel[3] = ld & (address[11:9] == 3'b011);
-    assign ld_sel[4] = ld & (address[11:9] == 3'b100);
-    assign ld_sel[5] = ld & (address[11:9] == 3'b101);
-    assign ld_sel[6] = ld & (address[11:9] == 3'b110);
-    assign ld_sel[7] = ld & (address[11:9] == 3'b111);
+    assign ld_sel[0] = ld && (address[11:9] == 3'b000);
+    assign ld_sel[1] = ld && (address[11:9] == 3'b001);
+    assign ld_sel[2] = ld && (address[11:9] == 3'b010);
+    assign ld_sel[3] = ld && (address[11:9] == 3'b011);
+    assign ld_sel[4] = ld && (address[11:9] == 3'b100);
+    assign ld_sel[5] = ld && (address[11:9] == 3'b101);
+    assign ld_sel[6] = ld && (address[11:9] == 3'b110);
+    assign ld_sel[7] = ld && (address[11:9] == 3'b111);
     
     RAM512 R5120(.clk(clk), .in(in), .address(address[8:0]), .ld(ld_sel[0]), .out(outs[0]));
     RAM512 R5121(.clk(clk), .in(in), .address(address[8:0]), .ld(ld_sel[1]), .out(outs[1]));
@@ -104,14 +107,14 @@ module RAM512(
     wire [15:0] outs [0:7];
     wire ld_sel [0:7];
     
-    assign ld_sel[0] = ld & (address[8:6] == 3'b000);
-    assign ld_sel[1] = ld & (address[8:6] == 3'b001);
-    assign ld_sel[2] = ld & (address[8:6] == 3'b010);
-    assign ld_sel[3] = ld & (address[8:6] == 3'b011);
-    assign ld_sel[4] = ld & (address[8:6] == 3'b100);
-    assign ld_sel[5] = ld & (address[8:6] == 3'b101);
-    assign ld_sel[6] = ld & (address[8:6] == 3'b110);
-    assign ld_sel[7] = ld & (address[8:6] == 3'b111);
+    assign ld_sel[0] = ld && (address[8:6] == 3'b000);
+    assign ld_sel[1] = ld && (address[8:6] == 3'b001);
+    assign ld_sel[2] = ld && (address[8:6] == 3'b010);
+    assign ld_sel[3] = ld && (address[8:6] == 3'b011);
+    assign ld_sel[4] = ld && (address[8:6] == 3'b100);
+    assign ld_sel[5] = ld && (address[8:6] == 3'b101);
+    assign ld_sel[6] = ld && (address[8:6] == 3'b110);
+    assign ld_sel[7] = ld && (address[8:6] == 3'b111);
     
     RAM64 R640(.clk(clk), .in(in), .address(address[5:0]), .ld(ld_sel[0]), .out(outs[0]));
     RAM64 R641(.clk(clk), .in(in), .address(address[5:0]), .ld(ld_sel[1]), .out(outs[1]));
@@ -147,7 +150,7 @@ module RAM64(
     genvar i;
     generate
       for (i = 0; i < 8; i = i + 1) begin : RAM_BANKS
-        assign ld_sel[i] = ld & (address[5:3] == i);
+        assign ld_sel[i] = ld && (address[5:3] == i);
       end
     endgenerate
     
@@ -173,14 +176,14 @@ module RAM8(
     wire [15:0] outs [0:7];
     wire ld_sel [0:7];
     
-    assign ld_sel[0] = ld & (address == 3'b000);
-    assign ld_sel[1] = ld & (address == 3'b001);
-    assign ld_sel[2] = ld & (address == 3'b010);
-    assign ld_sel[3] = ld & (address == 3'b011);
-    assign ld_sel[4] = ld & (address == 3'b100);
-    assign ld_sel[5] = ld & (address == 3'b101);
-    assign ld_sel[6] = ld & (address == 3'b110);
-    assign ld_sel[7] = ld & (address == 3'b111);
+    assign ld_sel[0] = ld && (address == 3'b000);
+    assign ld_sel[1] = ld && (address == 3'b001);
+    assign ld_sel[2] = ld && (address == 3'b010);
+    assign ld_sel[3] = ld && (address == 3'b011);
+    assign ld_sel[4] = ld && (address == 3'b100);
+    assign ld_sel[5] = ld && (address == 3'b101);
+    assign ld_sel[6] = ld && (address == 3'b110);
+    assign ld_sel[7] = ld && (address == 3'b111);
     
     Register R0(.clk(clk), .in(in), .ld(ld_sel[0]), .out(outs[0]));
     Register R1(.clk(clk), .in(in), .ld(ld_sel[1]), .out(outs[1]));
