7b773d978401a1a73e7743e561c6c9789ae7d1bb
FrancisRohith <166921745+FrancisRohith@users.noreply.github.com>
2025-08-09 11:27:11 +0530

    Update HACK.v

--- HACK_computer (nand2tetis)/HACK.v
+++ HACK_computer (nand2tetis)/HACK.v
@@ -3,10 +3,10 @@ module HACK(
     input rst
     );
     wire [15:0] inM, instr, outM, pc;
-    wire [14:0] addressM;
+    wire [15:0] addressM;
     wire writeM;
     Memory mem(.clk(clk), .in(outM), .address(addressM), .ld(writeM), .out(inM));
     CPU cpu(.clk(clk), .inM(inM), .instr(instr), .rst(rst), .writeM(writeM), .addressM(addressM), .outM(outM), .pc(pc));
-    ROM32K R32K(.clk(clk), .address(pc[14:0]), .out(instr)); 
+    ROM32K R32K(.clk(clk), .address(pc), .out(instr)); 
     
 endmodule
